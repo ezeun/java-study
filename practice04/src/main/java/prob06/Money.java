99d553e9b583af4bcaa1a6fb6e237d98c9060f0e
ezeun <consistency1231@gmail.com>
2024-11-25 17:38:07 +0900

    practice04/prob06

--- practice04/src/main/java/prob06/Money.java
+++ practice04/src/main/java/prob06/Money.java
@@ -1,7 +1,36 @@
 package prob06;
 
+import java.util.Objects;
+
 public class Money {
 	private int amount;
 	
-	/* 코드 작성 */
+	public Money(int amount) {
+		this.amount = amount;
+	}
+	
+	public Money add(Money m) {
+		return new Money(this.amount + m.amount);
+	}
+	public Money minus(Money m) {
+		return new Money(this.amount - m.amount);
+	}
+	public Money multiply(Money m) {
+		return new Money(this.amount * m.amount);
+	}
+	public Money divide(Money m) {
+		return new Money(this.amount / m.amount);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Money other = (Money) obj;
+		return amount == other.amount;
+	}
 }
