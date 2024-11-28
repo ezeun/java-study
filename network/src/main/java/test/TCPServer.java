10e214f977fb29b0d95d70809ae48b0e1a053ce5
ezeun <consistency1231@gmail.com>
2024-11-28 09:52:22 +0900

    TCP Server #2

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -24,23 +24,38 @@ public class TCPServer {
 			// 3. accept
 			Socket socket = serverSocket.accept(); //blocking
 			
-			System.out.println("연결 성공");
-			
-			// 4. IO Stream 받아오기
-			InputStream is = socket.getInputStream();
-			
-			// 5. 데이터 읽기
-			byte[] buffer = new byte[256];
-			int readByteCount = is.read(buffer); //blocking
-			if(readByteCount == -1) {
-				// closed by client
-				System.out.println("[server] closed by client");
-				return;
+			try {
+				InetSocketAddress inetRemoteSocetAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
+				String remoteHostAddress = inetRemoteSocetAddress.getAddress().getHostAddress();
+				int remotePort = inetRemoteSocetAddress.getPort();
+				
+				System.out.println("[server] connected by client["+remoteHostAddress+":"+remotePort+"]");				
+//				System.out.println("연결 성공"); //telnet 127.0.0.1 5001로 attack
+				
+				// 4. IO Stream 받아오기
+				InputStream is = socket.getInputStream();
+				
+				// 5. 데이터 읽기
+				byte[] buffer = new byte[256];
+				int readByteCount = is.read(buffer); //blocking
+				if(readByteCount == -1) {
+					// closed by client
+					System.out.println("[server] closed by client");
+					return;
+				}
+				
+				String data = new String(buffer, 0, readByteCount, "utf-8");
+				System.out.println("[server] receive:" + data);
+			} catch(IOException e) {
+				System.out.println("error:" +e);
+			} finally {
+				try {
+					if(serverSocket != null && !serverSocket.isClosed())
+						socket.close();
+				} catch (IOException e){
+					e.printStackTrace();
+				}
 			}
-			
-			String data = new String(buffer, 0, readByteCount, "utf-8");
-			System.out.println("[server] receive:" + data);
-			
 		} catch (IOException e) {
 			System.out.println("[server] error:"+e);
 		} finally {
