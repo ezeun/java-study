a70cbb984c5df27652a8a3130204d9873804995e
ezeun <consistency1231@gmail.com>
2024-11-28 13:40:30 +0900

    Echo Client #2

--- network/src/main/java/echo/EchoClient.java
+++ network/src/main/java/echo/EchoClient.java
@@ -1,45 +1,55 @@
 package echo;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.Scanner;
 
 public class EchoClient {
 	private static final String SERVER_IP = "192.168.0.25";
 	
 	public static void main(String[] args) {
+		Scanner scanner = null;
 		Socket socket = null;
 		
 		try {
+			scanner = new Scanner(System.in);
 			socket = new Socket();
 			
 			socket.connect(new InetSocketAddress(SERVER_IP, EchoServer.PORT));
 		
-			InputStream is = socket.getInputStream();
-			OutputStream os = socket.getOutputStream();
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "utf-8"), true);
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
 			
-			String data = "Hello World";
-			os.write(data.getBytes("utf-8"));
-			
-			byte[] buffer = new byte[256];
-			int readByteCount = is.read(buffer);
-			if(readByteCount == -1) {
-				log("closed by server");
-				return;
+			while(true) {
+				System.out.print(">>");
+				String line = scanner.nextLine();
+				
+				if("exit".equals(line)) {
+					break;
+				}
+				
+				pw.println(line);
+				String data = br.readLine();
+				if(data == null) {
+					log("closed by server");
+					break;
+				}
+				System.out.println("<<" + data);
 			}
-			
-			data = new String(buffer, 0, readByteCount, "utf-8");
-			log("received:"+data);
-			
 		}catch(SocketException e) {
 			log("Socket Exception" +e);
 		} catch (IOException e) {
 			log("error:"+e);
 		} finally {
 			try {
+				if(scanner != null)
+					scanner.close();
 				if(socket!=null && !socket.isClosed())
 					socket.close();
 			} catch (IOException e) {
