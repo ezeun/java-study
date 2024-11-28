2406b3ab8e6de5c91dd50cef665bae6845481247
ezeun <consistency1231@gmail.com>
2024-11-28 10:59:58 +0900

    TCP Client #1

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -2,6 +2,7 @@ package test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -34,18 +35,24 @@ public class TCPServer {
 				
 				// 4. IO Stream 받아오기
 				InputStream is = socket.getInputStream();
+				OutputStream os = socket.getOutputStream();
 				
-				// 5. 데이터 읽기
-				byte[] buffer = new byte[256];
-				int readByteCount = is.read(buffer); //blocking
-				if(readByteCount == -1) {
-					// closed by client
-					System.out.println("[server] closed by client");
-					return;
+				while(true) {
+					// 5. 데이터 읽기
+					byte[] buffer = new byte[256];
+					int readByteCount = is.read(buffer); //blocking
+					if(readByteCount == -1) {
+						// closed by client
+						System.out.println("[server] closed by client");
+						break;
+					}
+					
+					String data = new String(buffer, 0, readByteCount, "utf-8");
+					System.out.println("[server] receive:" + data);
+					
+					// 6. 데이터 쓰기 (client에게 전달)
+					os.write(data.getBytes("utf-8"));
 				}
-				
-				String data = new String(buffer, 0, readByteCount, "utf-8");
-				System.out.println("[server] receive:" + data);
 			} catch(IOException e) {
 				System.out.println("error:" +e);
 			} finally {
